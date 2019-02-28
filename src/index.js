4922cffe02675ea77821826837fc772e0aa70aac
Harendranath Vegi <ng2vrp@gmail.com>
2019-02-28 17:01:24 +0530

    Basics of Reactjs Executed and JSX usage also Implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from  'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ const tasks = ['take out the trash', 'take out the trash', 'walk the dog'];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = React.createElement('ol',null,
+// React.createElement('li', null, 'TakeOut  the Trash'),
+// React.createElement('li', null, 'Shovel the driveway'),
+// React.createElement('li', null, 'walk the dog'),
+// tasks.map((task, index ) => React.createElement('li' , {key: index}, task))
+   
+// );
+
+// const element = <ol>
+//      <li>Hello Vegi</li>
+//      <li>Hello Harendra</li>
+//      <li>Hello Nath</li><hr></hr>
+//      <li>Hello VegiHarendranath!!!</li>
+// </ol>
+
+
+
+
+//  const element = <ol>
+//     <li>{tasks[0]}</li>
+//     <li>{tasks[1]}</li>
+//     <li>{tasks[2]}</li><hr></hr>
+     
+//  </ol>
+
+// const element = <ol>
+//     {tasks.map((task) => <li key = {task}> {task}</li>)}
+// </ol>
+
+const element = <div>
+    <h1> TaskList </h1><ol>
+    {tasks.map((task, index) => <li key = {index}> {task}</li>)}
+</ol>
+</div>
+
+ReactDOM.render(element, document.getElementById('root'));
+
+
